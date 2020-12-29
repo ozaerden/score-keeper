867d457dcee66b884d5692d59a6cd31cadbc0831
Ozan Erden <ozan.erden@primeapps.io>
2020-12-29 20:19:27 +0300

    Design improvements

--- script.js
+++ script.js
@@ -20,33 +20,38 @@ team1button.addEventListener('click', function(){
         t1score += 1;
         if(t1score === winningScore){ 
             isGameOver = true;
-            team1score.classList.add('winner');
-            team2score.classList.add('loser');
+            team1score.classList.add('has-text-success');
+            team2score.classList.add('has-text-danger');
+            team1button.disabled = true;
+            team2button.disabled = true;
         }
         team1score.textContent = t1score;
     }
 })
 
-team2button.addEventListener('click', function(){
-    if(!isGameOver){
-        t2score += 1;
-        if(t2score === winningScore){ 
-            isGameOver = true;
-            team2score.classList.add('winner');
-            team1score.classList.add('loser');
-        }
-        team2score.textContent = t2score;
-    }
-})
-
 resetButton.addEventListener('click', reset)
-
 function reset(){
     isGameOver = false;
     t1score = 0;
     t2score = 0;
     team1score.textContent = t1score;
     team2score.textContent = t2score;
-    team1score.classList.remove('winner', 'loser');
-    team2score.classList.remove('winner', 'loser');
+    team1score.classList.remove('has-text-success', 'has-text-danger');
+    team2score.classList.remove('has-text-success', 'has-text-danger');
+    team1button.disabled = false;
+    team2button.disabled = false;
 }
+
+team2button.addEventListener('click', function(){
+    if(!isGameOver){
+        t2score += 1;
+        if(t2score === winningScore){ 
+            isGameOver = true;
+            team2score.classList.add('has-text-success');
+            team1score.classList.add('has-text-danger');
+            team1button.disabled = true;
+            team2button.disabled = true;
+        }
+        team2score.textContent = t2score;
+    }
+})
