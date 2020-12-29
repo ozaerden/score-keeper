3a1301d572f763b4a04121e9d238ea27a3be01cb
Ozan Erden <ozan.erden@primeapps.io>
2020-12-29 15:46:50 +0300

    Winning limits and colors added

--- script.js
+++ script.js
@@ -3,17 +3,25 @@ const team2score = document.querySelector('#team2score');
 const team1button = document.querySelector('#team1button');
 const team2button = document.querySelector('#team2button');
 const resetButton = document.querySelector("#resetButton");
+const scoreSelect = document.querySelector("#playTo");
 
 let t1score = 0;
 let t2score = 0;
-let winningScore = 5;
+let winningScore = 1;
 let isGameOver = false;
+
+scoreSelect.addEventListener('change', function(){
+    winningScore = parseInt(this.value);
+    reset();
+})
 
 team1button.addEventListener('click', function(){
     if(!isGameOver){
         t1score += 1;
         if(t1score === winningScore){ 
             isGameOver = true;
+            team1score.classList.add('winner');
+            team2score.classList.add('loser');
         }
         team1score.textContent = t1score;
     }
@@ -24,15 +32,21 @@ team2button.addEventListener('click', function(){
         t2score += 1;
         if(t2score === winningScore){ 
             isGameOver = true;
+            team2score.classList.add('winner');
+            team1score.classList.add('loser');
         }
         team2score.textContent = t2score;
     }
 })
 
-resetButton.addEventListener('click', function(){
+resetButton.addEventListener('click', reset)
+
+function reset(){
     isGameOver = false;
     t1score = 0;
     t2score = 0;
     team1score.textContent = t1score;
     team2score.textContent = t2score;
-})
+    team1score.classList.remove('winner', 'loser');
+    team2score.classList.remove('winner', 'loser');
+}
